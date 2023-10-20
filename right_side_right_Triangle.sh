584a9d9259d4f54b81653083309eaf36dcf984da
swapnilnirmal09 <147571910+swapnilnirmal09@users.noreply.github.com>
2023-10-20 17:49:43 +0530

    Add files via upload

---
+++ right_side_right_Triangle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Enter no. of rows : " num
+
+for ((i=1;i<=num;i++))
+do
+for((j=1;j<=num-i;j++))
+do
+echo -n " "
+done
+for ((k=1;k<=i;k++))
+do
+echo -n "*"
+done
+echo
+done
